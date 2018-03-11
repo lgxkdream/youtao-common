57f344534644d9bbc88e20d629dab91dd9b997b5
Jackie Lee <lgxkdream@foxmail.com>
2018-03-11 23:06:07 +0800

    feat: cookie工具类

--- src/main/java/com/youtao/common/spring/exetend/converter/json/CallbackMappingJackson2HttpMessageConverter.java
+++ src/main/java/com/youtao/common/spring/exetend/converter/json/CallbackMappingJackson2HttpMessageConverter.java
@@ -1,4 +1,5 @@
 package com.youtao.common.spring.exetend.converter.json;
+
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,36 +17,38 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class CallbackMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
-	// 做jsonp的支持的标识，在请求参数中加该参数
-	private String callbackName;
+    // 做jsonp的支持的标识，在请求参数中加该参数
+    private String callbackName;
 
-	@Override
-	protected void writeInternal(Object object, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-		// 从threadLocal中获取当前的Request对象
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-		String callbackParam = request.getParameter(callbackName);
-		if(StringUtils.isEmpty(callbackParam)){
-			// 没有找到callback参数，直接返回json数据
-			super.writeInternal(object, outputMessage);
-		}else{
-			JsonEncoding encoding = getJsonEncoding(outputMessage.getHeaders().getContentType());
-			try {
-				String result =callbackParam+"("+super.getObjectMapper().writeValueAsString(object)+");";
-				IOUtils.write(result, outputMessage.getBody(),encoding.getJavaName());
-			}
-			catch (JsonProcessingException ex) {
-				throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
-			}
-		}
-		
-	}
+    @Override
+    protected void writeInternal(Object object, HttpOutputMessage outputMessage) throws IOException,
+            HttpMessageNotWritableException {
+        // 从threadLocal中获取当前的Request对象
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                .currentRequestAttributes()).getRequest();
+        String callbackParam = request.getParameter(callbackName);
+        if (StringUtils.isEmpty(callbackParam)) {
+            // 没有找到callback参数，直接返回json数据
+            super.writeInternal(object, outputMessage);
+        } else {
+            JsonEncoding encoding = getJsonEncoding(outputMessage.getHeaders().getContentType());
+            try {
+                String result = callbackParam + "(" + super.getObjectMapper().writeValueAsString(object)
+                        + ");";
+                IOUtils.write(result, outputMessage.getBody(), encoding.getJavaName());
+            } catch (JsonProcessingException ex) {
+                throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
+            }
+        }
 
-	public String getCallbackName() {
-		return callbackName;
-	}
+    }
 
-	public void setCallbackName(String callbackName) {
-		this.callbackName = callbackName;
-	}
+    public String getCallbackName() {
+        return callbackName;
+    }
+
+    public void setCallbackName(String callbackName) {
+        this.callbackName = callbackName;
+    }
 
 }
